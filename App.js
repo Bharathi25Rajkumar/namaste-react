da9a8a6c4f291a8826267b479c504e86419e4578
Bharathi Rajkumar <bharathi_rajkumar@epam.com>
2026-02-18 18:43:54 +0530

    epdisode-02

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 {/* <div id="parent">
     <div id="child1">
-        <h1>I'm a h1 tag</h1>
+        <h1>This is a Namaste React course</h1>
         <h2>I'm a h2 tag</h2>
     </div>
 
@@ -13,8 +16,8 @@
 
 const parent = React.createElement("div", {id: "parent"}, [
     React.createElement("div", {id: "child1"}, [
-        React.createElement("h1", {}, "I'm a h1 tag"),
-        React.createElement("h2", {}, "I'm a h2 tag")
+        React.createElement("h1", {}, "This is a Namaste React course"),
+        React.createElement("h2", {}, "by Akshay Saini")
     ]),
     React.createElement("div", {id: "child2"}, [
         React.createElement("h1", {}, "I'm a h1 tag"),
