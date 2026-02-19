578f4c06028c818276ed3f203c6e961eee70609f
Bharathi Rajkumar <bharathi_rajkumar@epam.com>
2026-02-19 18:28:06 +0530

    additional changes

--- App.js
+++ App.js
@@ -13,11 +13,18 @@ const Title = () => {
     return <h1 className="title">Welcome to React Course</h1>;
 }
 
+const dats = AudioParam.getData();
+
+//Component Composition - Component inside a component
 const HeadingComponent = () => {
     return (
         <div id="heading">
             {header}
+            {data}  
+            {/* JS prevents Cross-site scripting attack by sanitizing the data, so that we can directly use it */}
             <Title />
+            <Title></Title>
+            {Title()}
             <div>
                 <h2>This is a React Course for Benginners</h2>
             </div>
