c9e8552573b512249b3a8917bae7616ac83d7a58
Bharathi Rajkumar <bharathi_rajkumar@epam.com>
2026-02-19 15:00:22 +0530

    react element and react functional component diff

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id: "heading"}, "This is a React Fundamentals course");
+const Heading = () => true;
+const HeadingFn = () => {
+    return true;
+}
 
-const jsxHeading = 
-<div>
-<h1 className="heading" tabIndex={1}>Bharu is here</h1>
-</div>;
+const header = <h1>This is a header</h1>;
+
+
+const Title = () => {
+    return <h1 className="title">Welcome to React Course</h1>;
+}
+
+const HeadingComponent = () => {
+    return (
+        <div id="heading">
+            {header}
+            <Title />
+            <div>
+                <h2>This is a React Course for Benginners</h2>
+            </div>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
